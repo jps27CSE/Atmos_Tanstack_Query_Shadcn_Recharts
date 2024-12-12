be4a638616850e8c17419720266462bd493dd925
jps27CSE <jackpritomsoren@hotmail.com>
2024-12-12 12:28:18 +0600

    add: dark-mode, weather-dashboard page and city-page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "@/components/layout.tsx";
+import { ThemeProvider } from "@/context/theme-provider.tsx";
+import WeatherDashboard from "@/pages/weather-dashboard.tsx";
+import CityPage from "@/pages/city-page.tsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>Hello</Layout>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
