14348c1305833ea37855ef9357dd087194a3625b
jps27CSE <jackpritomsoren@hotmail.com>
2024-12-18 14:42:23 +0600

    use-geolocation hook (in progress)

---
+++ src/hooks/use-geolocation.tsx
@@ -0,0 +1,38 @@
+import {useEffect, useState} from "react";
+import { Coordinates } from "@/api/types.ts";
+
+interface GeolocationState {
+  coordinates: Coordinates | null;
+  error: string | null;
+  isLoading: boolean;
+}
+
+export function useGeolocation() {
+  const [locationData, setLocationData] = useState<GeolocationState>({
+    coordinates: null,
+    error: null,
+    isLoading: true,
+  });
+
+  const getLocation(){
+      setLocationData((prev)=>({...prev,isLoading:true,error:null}))
+
+        if(!navigator.geolocation){
+            setLocationData({
+                coordinates:null,
+                error:'Geolocation is not supported by your browser',
+                isLoading:false
+            })
+        }
+
+    }
+
+    useEffect(() => {
+        getLocation()
+    }, []);
+
+  return{
+      ...locationData,  getLocation
+  }
+
+}
--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import { Button } from "@/components/ui/button.tsx";
+import { RefreshCw } from "lucide-react";
 
 const WeatherDashboard = () => {
-  return <div>WeatherDashboard</div>;
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button variant={"outline"} size={"icon"}>
+          <RefreshCw className="h-4 w-4" />
+        </Button>
+      </div>
+    </div>
+  );
 };
 export default WeatherDashboard;
